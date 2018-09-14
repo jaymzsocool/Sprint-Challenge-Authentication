37446636c3ece3c36ed6f05091c29bdce50e408c
jaymzsocool <james.mcraejr@gmail.com>
2018-09-14 12:14:43 -0400

    all routes working

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig');
 
-const { authenticate } = require('./middlewares');
+const { authenticate, generateToken } = require('./middlewares');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -8,12 +10,39 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+
+
 function register(req, res) {
-  // implement user registration
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 10)
+  creds.password = hash;
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0]
+      db('users')
+        .where({ id })
+        .first()
+        .then(user => {
+          const token = generateToken(user);
+          res.status(201).json({ id: user.id, token });
+        }).catch(err => res.status(500).send(err));
+    }).catch(err=> res.status(500).send(err))
 }
 
 function login(req, res) {
-  // implement user login
+  const creds = req.body;  
+  db('users')
+    .where({username: creds.username})
+    .first()
+    .then(user =>{
+      if (user && bcrypt.compareSync(creds.password, user.password)){
+        const token = generateToken(user);
+        res.status(200).send({token})
+      } else {
+        res.status(401).json({message: 'The username or password incorrect.'})
+      }
+    })
 }
 
 function getJokes(req, res) {
--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const { server } = require('./server.js');
+const db = require('./database/dbConfig');
+
+server.get('/api/users', (req, res) => {    
+  db('users')
+      .select('id', 'username')
+      .then(users => {
+          res.json(users);
+      })
+      .catch(err => res.send(err));    
+});
 
 const port = process.env.PORT || 3300;
 server.listen(port, () => {
